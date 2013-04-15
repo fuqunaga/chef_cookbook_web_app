8dde3186c9f736c4b34a85f66948c0c13242ff41
fuqunaga <fukhid+github@gmail.com>
2013-04-16 00:44:26 +0900

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,15 @@
+include_recipe "apache2"
+web_app "web_app" do
+  docroot "/vagrant/www"
+  template "web_app.conf.erb"
+  server_name node[:fqdn]
+  server_aliases ["web"]
+end
+
+package "pcre-devel" do
+	action :install
+end
+
+php_pear "oauth" do
+	action :install
+end
