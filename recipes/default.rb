b1a66f8e26986723513822900745ab623ef9c514
fukunaga <fukhid@gmail.com>
2013-04-25 21:48:08 +0900

    add APC install

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,8 @@ include_recipe "apache2"
 web_app "web_app" do
   docroot "/vagrant/www"
   template "web_app.conf.erb"
-  server_name node[:fqdn]
+  #server_name node[:fqdn]
+  server_name "fukunaga.imas.swdev.jp"
   server_aliases ["web"]
 end
 
@@ -11,5 +12,9 @@ package "pcre-devel" do
 end
 
 php_pear "oauth" do
+	action :install
+end
+
+php_pear "APC" do
 	action :install
 end
